f07cad5bcf5b88c17f6d1d49b8727b057f5d40a7
VuTanTai <tantai12131@gmail.com>
2026-07-29 16:38:39 +0700

    Xử lí DATE

--- sql_file/1.Xu_li.sql
+++ sql_file/1.Xu_li.sql
@@ -1,12 +1,35 @@
 SELECT * FROM consumer_complaints
 
 
+----XỬ LÍ các cột date_received và date_resolved
+--XÓA 2 CỘT dư là date_received_2 và date_resolved_2
+ALTER TABLE consumer_complaints
+DROP COLUMN  date_received_2
 
--XÓA 2 CỘT date_received_2 và date_resolved_2
--ĐỔI TÊN date_received_1 và date_resolved_1 bỏ số đuôi
--Đổi định dạng 2 cột trên thành yyyy-mm-dd
+ALTER TABLE consumer_complaints
+DROP COLUMN  date_resolved_2
 
 
+--ĐỔI TÊN date_received_1 và date_resolved_1 bỏ số đuôi
+ALTER TABLE consumer_complaints
+RENAME COLUMN date_received_1 TO date_received
+
+ALTER TABLE consumer_complaints
+RENAME COLUMN date_resolved_1 TO date_resolved
+
+
+--Đổi định dạng 2 cột trên thành yyyy-mm-dd
+ALTER TABLE consumer_complaints
+ALTER COLUMN date_received TYPE DATE
+USING TO_DATE(date_received, 'MM-DD-YYYY')
+
+ALTER TABLE consumer_complaints
+ALTER COLUMN date_received TYPE DATE
+USING TO_DATE(date_resolved, 'MM-DD-YYYY')
+
+
+SELECT COUNT(*)
+FROM consumer_complaints
 
 
 
